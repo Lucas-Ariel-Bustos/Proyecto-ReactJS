695ea1e9bdbea514483151a046ab8f6d8234bae3
Lucas Ariel Bustos <lucasbustos208@gmail.com>
2023-02-19 16:52:37 -0300

    PreEntrega1+BustosLucas

--- src/Componentes/CartWidget/CartWidget.js
+++ src/Componentes/CartWidget/CartWidget.js
@@ -5,6 +5,7 @@ export const CartWidget = () => {
     
     return (
         <div className="divCart">
+            {/* <i class="bi bi-cart4"></i> */}
             <img className="carrito" src={carrito}></img>
         </div>
     )
